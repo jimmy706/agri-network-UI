7b31e7fef99375302f4f8660c04da3b3e44ed99f
Dung Dang <jim.dangquoc706@gmail.com>
2021-08-25 23:44:35 +0700

    add login activity

--- app/src/main/java/com/agrinetwork/MainActivity.java
+++ app/src/main/java/com/agrinetwork/MainActivity.java
@@ -1,14 +1,105 @@
 package com.agrinetwork;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.agrinetwork.auth.Register;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Objects;
+
+public class MainActivity extends Activity {
+
+    private static final int RC_SIGN_IN = 1;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton loginButton = findViewById(R.id.login_btn);
+        TextInputEditText usernameInput = findViewById(R.id.edit_text_username);
+        TextInputEditText passwordInput = findViewById(R.id.edit_text_password);
+        TextView moveToRegisterLink = findViewById(R.id.move_to_register);
+
+        moveToRegisterLink.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Register.class);
+            startActivity(intent);
+        });
+
+        loginButton.setOnClickListener(view -> {
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+
+            Toast.makeText(this, username + " - " + password, Toast.LENGTH_SHORT).show();
+        });
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(Objects.nonNull(account)) {
+            redirectToFeedActivity();
+        }
+
+        SignInButton ggSignInButton = findViewById(R.id.gg_sign_in_button);
+        ggSignInButton.setOnClickListener(view -> {
+            Intent ggSignInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(ggSignInIntent, RC_SIGN_IN);
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleGgSignInResult(task);
+        }
+    }
+
+    private void handleGgSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+        }
+        catch (ApiException e) {
+            Log.w(Integer.toString(e.getStatusCode()), "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+    private void redirectToFeedActivity() {
+
     }
 }
---
+++ app/src/main/java/com/agrinetwork/auth/Register.java
@@ -0,0 +1,23 @@
+package com.agrinetwork.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.agrinetwork.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+
+    }
+}
