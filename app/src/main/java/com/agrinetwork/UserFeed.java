6ddd657c154134d8bb16ddab64371513b36ecbe6
Dung Dang <jim.dangquoc706@gmail.com>
2021-09-05 22:29:38 +0700

    update bottom bar and add new post fragment

--- app/src/main/java/com/agrinetwork/UserFeed.java
+++ app/src/main/java/com/agrinetwork/UserFeed.java
@@ -1,7 +1,9 @@
 package com.agrinetwork;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.agrinetwork.databinding.ActivityUserFeedBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class UserFeed extends AppCompatActivity {
 
@@ -22,15 +25,30 @@ public class UserFeed extends AppCompatActivity {
 
         binding = ActivityUserFeedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        View viewFragment = findViewById(R.id.nav_host_fragment_activity_user_feed);
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+        viewFragment.setPadding(0,0,0,bottomAppBar.getHeight());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.getMenu().getItem(2).setEnabled(false); // Disable center item so that floating button can click
+
+
+        FloatingActionButton btnAddPost = findViewById(R.id.btn_add_post);
+        btnAddPost.bringToFront();
+
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_networks, R.id.navigation_products, R.id.navigation_notifications)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_user_feed);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        btnAddPost.setOnClickListener(v -> {
+            navController.navigate(R.id.navigation_add_post);
+        });
     }
 
 }
