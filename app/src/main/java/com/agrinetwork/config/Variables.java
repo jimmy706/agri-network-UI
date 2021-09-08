a4d43a34d11fa1f6374fb886c79aa789f5ea4530
Dung Dang <jim.dangquoc706@gmail.com>
2021-09-09 00:12:07 +0700

    add service methods to call APIs

--- app/src/main/java/com/agrinetwork/config/Variables.java
+++ app/src/main/java/com/agrinetwork/config/Variables.java
@@ -5,6 +5,7 @@ public class Variables {
     public static final String ID_TOKEN_LABEL = "idToken";
     public static final String SHARED_TOKENS = "shared_tokens";
     public static final String UPLOAD_MEDIA_TOKEN = "e9484013d4593fb6727bdff4580f7f4d";
+    public static final int DEFAULT_LIMIT_POST = 6;
 
     private Variables() {
 
--- app/src/main/java/com/agrinetwork/service/PostService.java
+++ app/src/main/java/com/agrinetwork/service/PostService.java
@@ -1,10 +1,17 @@
 package com.agrinetwork.service;
 
 import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 
 import com.agrinetwork.config.Variables;
 import com.agrinetwork.entities.PostItem;
+import com.agrinetwork.helpers.UrlHelper;
 import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.MediaType;
@@ -15,7 +22,7 @@ import okhttp3.RequestBody;
 public class PostService {
     private final Context context;
     private final OkHttpClient client = new OkHttpClient();
-    private static final String SERVICCE_URL = Variables.SERVICE_DOMAIN + "/posts";
+    private static final String SERVICE_URL = Variables.SERVICE_DOMAIN + "/posts";
 
     public PostService(Context context) {
         this.context = context;
@@ -27,7 +34,7 @@ public class PostService {
         RequestBody body = RequestBody.create(json, MediaType.parse("application/json"));
 
         Request request = new Request.Builder()
-                .url(SERVICCE_URL)
+                .url(SERVICE_URL)
                 .header("Authorization", token)
                 .post(body)
                 .build();
@@ -35,4 +42,47 @@ public class PostService {
         return client.newCall(request);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Call getPostsFromUser(String token, String uid, Integer page, Integer limit) {
+        UrlHelper urlHelper = new UrlHelper();
+
+        Map<String, Integer> params = new HashMap<>();
+        params.put("page", page);
+        params.put("limit", limit);
+
+        Request request = new Request.Builder()
+                .header("Authorization", token)
+                .url(SERVICE_URL + "/owner/" + uid + "?" + urlHelper.convertFromMapToQueryString(params))
+                .get()
+                .build();
+
+        return client.newCall(request);
+    }
+
+    public Call getCommentsAndReactionsCountFromPost(String postId){
+        Request request = new Request.Builder()
+                .url(SERVICE_URL + "/" + postId + "/commentsAndReactionsCount")
+                .get()
+                .build();
+
+        return client.newCall(request);
+    }
+
+    public Call getPostDetail(String postId) {
+        Request request = new Request.Builder()
+                .url(SERVICE_URL + "/" + postId)
+                .get()
+                .build();
+
+        return client.newCall(request);
+    }
+
+    public Call deletePost(String postId) {
+        Request request = new Request.Builder()
+                .url(SERVICE_URL + "/" + postId)
+                .delete()
+                .build();
+
+        return client.newCall(request);
+    }
 }
