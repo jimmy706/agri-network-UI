b5be45648b3847aa56e39818d9331e0295c99842
maivanday <maivanday90@gmail.com>
2021-10-07 15:36:08 +0700

    add category

---
+++ app/src/main/java/com/agrinetwork/entities/ProductCategory.java
@@ -0,0 +1,10 @@
+package com.agrinetwork.entities;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProductCategory {
+    String name;
+
+}
--- app/src/main/java/com/agrinetwork/ui/products/ProductsFragment.java
+++ app/src/main/java/com/agrinetwork/ui/products/ProductsFragment.java
@@ -1,66 +1,98 @@
 package com.agrinetwork.ui.products;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.agrinetwork.R;
+import com.agrinetwork.config.Variables;
+import com.agrinetwork.entities.ProductCategory;
+import com.agrinetwork.service.CategoryService;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProductsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+
 public class ProductsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProductsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProductsFragment newInstance(String param1, String param2) {
-        ProductsFragment fragment = new ProductsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ChipGroup chipGroup;
+    private CategoryService categoryService;
+    private String token;
+    private final Gson gson = new Gson();
+    private final List<ProductCategory> productCategoryList = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_products, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_products, container, false);
+        chipGroup = view.findViewById(R.id.category_group);
+        SharedPreferences sharedPref = getActivity().getSharedPreferences(Variables.SHARED_TOKENS, Context.MODE_PRIVATE);
+        token = sharedPref.getString(Variables.ID_TOKEN_LABEL, "");
+        categoryService = new CategoryService(getContext());
+
+        fetchCategory();
+        return  view;
     }
+
+    public  void fetchCategory(){
+        Call getAllCategory = categoryService.getCategory(token);
+        getAllCategory.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call getAllCategory, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onResponse(@NonNull Call getAllCategory, @NonNull Response response) throws IOException {
+                if(response.code() == 200){
+                    String responseCategory = response.body().string();
+
+                    Type categoryList = new TypeToken<List<ProductCategory>>(){}.getType();
+                    List<ProductCategory> productTypes = gson.fromJson(responseCategory,categoryList);
+
+                    getActivity().runOnUiThread(()->{
+                        productCategoryList.addAll(productTypes);
+
+                        for(int i = 0; i < productCategoryList.size(); i++){
+                            final ProductCategory category = productCategoryList.get(i);
+                            String categoryName = category.getName();
+                            final Chip chip = new Chip(getContext());
+                            chip.setText(categoryName);
+                            chip.setChipBackgroundColorResource(R.color.teal_200);
+                            chip.setTextSize(16);
+
+                            chipGroup.addView(chip);
+
+                        }
+
+                    });
+                }
+            }
+        });
+    }
+
 }
