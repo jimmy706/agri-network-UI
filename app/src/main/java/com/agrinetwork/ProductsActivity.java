623038ba0c67ce40570a08db73fb6098472fd028
Dung Dang <jim.dangquoc706@gmail.com>
2021-10-31 17:30:50 +0700

    Add navigation drawer to products activity

--- app/src/main/java/com/agrinetwork/ProductsActivity.java
+++ app/src/main/java/com/agrinetwork/ProductsActivity.java
@@ -1,14 +1,33 @@
 package com.agrinetwork;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
 public class ProductsActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.products_navigation_toggler, R.string.products_navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
